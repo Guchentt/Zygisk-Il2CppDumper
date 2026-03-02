e86b3c6db488b138aad88a0deb99aba7429beac4
Guchentt <tt666298@163.com>
2026-03-02 21:49:38 +0800

    Update network_hook.h

--- module/src/main/cpp/network_hook.h
+++ module/src/main/cpp/network_hook.h
@@ -10,10 +10,11 @@
 
 // Enable/disable network hooking
 // Set to 0 to disable network hooks (game will run normally)
-#define ENABLE_NETWORK_HOOK 0
+#define ENABLE_NETWORK_HOOK 1
 
 // Hook network methods to log network communication
 void hook_network_methods(void *il2cpp_handle);
 
 #endif //ZYGISK_IL2CPPDUMPER_NETWORK_HOOK_H
+
 
